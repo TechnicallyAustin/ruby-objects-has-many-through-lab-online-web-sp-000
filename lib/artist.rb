38c18a35a36b9997d13b84b00293a2c144cb5142
Austin Johnson <technicallyaustin@gmail.com>
2021-03-03 20:15:15 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+attr_accessor :name,
+@@all = []
+
+def initialize(name)
+  @name = name 
+  @songs = []
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def songs
+  @songs.select {|song| song.artist == self}
+end
+
+def new_song(name, genre)
+  Song.new(name,genre)
+  
+  
+end
